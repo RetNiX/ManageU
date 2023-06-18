cbeed5d0c008891ff3ff57a2d2f36b7c03c18802
RetNiX <kochick16@gmail.com>
2023-06-18 18:27:40 +0300

    Added localStorage handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,25 +7,28 @@ let manager = new ActionManager();
 function showActions() {
     document.getElementById("tableData").innerHTML = "";
     for (let action of manager.actions) {
-        document.getElementById("tableData").innerHTML += `<tr class=${action.type == "income" ? "table-success" : "table-danger"}><td>${action.description}</td><td>${action.amount}</td><td><i class="fa-solid fa-pen-to-square" onclick="updateAction(${action.id})" style="color: #5188e6;"></i></td><td><i class="fa-solid fa-trash-can" onclick="deleteAction(${action.id})" style="color: #5188e6;"></i></td></tr>`;
+        document.getElementById("tableData").innerHTML += `<tr class=${action.type == "income" ? "table-success" : "table-danger"}><td>${action.description}</td><td>${action.amount}</td><td><i id="icon" class="fa-solid fa-pen-to-square" onclick="updateAction(${action.id})" style="color: #5188e6;"></i></td><td><i id="icon" class="fa-solid fa-trash-can" onclick="deleteAction(${action.id})" style="color: #5188e6;"></i></td></tr>`;
     }
 }
 /* Updating actions each time you click ADD */
 window.addNewAction = function (){
-    /* Taking the values from the form */
-    let type = document.getElementById("typeBalance").value;
-    let desc = document.getElementById("desc").value;
-    let amount = +document.getElementById("amount").value;
-    /* Creating Action object */
-    let newAction = new Action(type,desc,amount);
-    /* Send to method addAction */
-    manager.addAction(newAction);
-    showNewAction();
-    console.log(manager.actions);
+        /* Taking the values from the form */
+        let type = document.getElementById("typeBalance").value;
+        let desc = document.getElementById("desc").value;
+        let amount = +document.getElementById("amount").value;
+        /* Creating Action object */
+        let newAction = new Action(type,desc,amount);
+        /* Send to method addAction */
+        manager.addAction(newAction);
+        showNewAction();
+        console.log(manager.actions);
+        /* Local Storage Adding */
+        var serializedTransactionJSON = JSON.stringify(newAction);
+        localStorage.setItem(`Transaction${newAction.id}`, serializedTransactionJSON);
 }
 /* Everytime we add a new Action, we add/remove 1 row instead using showActions */
 function showNewAction() {
-    document.getElementById("tableData").innerHTML += `<tr class=${manager.actions[manager.actions.length - 1].type == "income" ? "table-success" : "table-danger"}><td>${manager.actions[manager.actions.length - 1].description}</td><td>${manager.actions[manager.actions.length-1].amount}</td><td><i class="fa-solid fa-pen-to-square" onclick="updateAction(${manager.actions[manager.actions.length-1].id})" style="color: #5188e6;"></i></td><td><i class="fa-solid fa-trash-can" onclick="deleteAction(${manager.actions[manager.actions.length-1].id})" style="color: #5188e6;"></i></td></tr>`;
+    document.getElementById("tableData").innerHTML += `<tr class=${manager.actions[manager.actions.length - 1].type == "income" ? "table-success" : "table-danger"}><td>${manager.actions[manager.actions.length - 1].description}</td><td>${manager.actions[manager.actions.length-1].amount}</td><td><i id="icon" class="fa-solid fa-pen-to-square" onclick="updateAction(${manager.actions[manager.actions.length-1].id})" style="color: #5188e6;"></i></td><td><i id="icon" class="fa-solid fa-trash-can" onclick="deleteAction(${manager.actions[manager.actions.length-1].id})" style="color: #5188e6;"></i></td></tr>`;
 }
 
 /* Function for editing the amount of a selected action */
@@ -36,6 +39,10 @@ window.updateAction = function (id) {
         manager.updateAction(id, +newAmount);
         showActions();
     }
+    /* Local Storage Edit */
+    let tempStore = JSON.parse(localStorage.getItem(`Transaction${id}`));
+    tempStore.amount = +newAmount;
+    localStorage.setItem(`Transaction${id}`,JSON.stringify(tempStore));
 }
 /* Function for deleting a selected Action */
 window.deleteAction = function (id) {
@@ -44,6 +51,22 @@ window.deleteAction = function (id) {
         showActions();
         setTimeout(() => {
             alert("The transaction has been REMOVED.");
-        }, 200);
+        }, 150);
+    }
+    /* Local Storage Removal */
+    localStorage.removeItem(`Transaction${id}`);
+}
+
+window.disableSelect = function() {
+    var dropDown = document.getElementById("typeBalance").value;
+    var btn = document.getElementById("addBtn");
+    if (dropDown == "not") {
+        btn.disabled = true;
+        btn.style.cursor = "not-allowed"
+    }
+    else {
+        btn.disabled = false;
+        btn.style.cursor = "pointer";
     }
 }
+window.onload = disableSelect();
